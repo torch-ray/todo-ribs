a0ac81b3525f229136cc967358f77c6669bfddcf
wooseok.suh <wooseok.suh@woowahan.com>
2022-03-14 12:27:51 +0900

    프로젝트 생성

---
+++ todo-ribs/todo-ribs/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  todo-ribs
+//
+//  Created by wooseok.suh on 2022/03/14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
